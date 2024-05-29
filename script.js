c3e49c220b5389a018ff974e946a185b78505863
TimothyEniola <timothyyemitan@gmail.com>
2024-05-29 16:18:59 +0100

    my message

---
+++ script.js
@@ -0,0 +1,7 @@
+
+const shiloh = document.querySelector(".hambs")
+const redemption = document.querySelector(".joom")
+function lamb() {
+  redemption.classList.toggle('joom')
+}
+shiloh.addEventListener('click', lamb);
